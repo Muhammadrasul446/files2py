1d7796d34d26ef3268a0a7e544a291bc9b205495
N0n3 <52095116+N0n3-github@users.noreply.github.com>
2019-08-22 00:45:39 +0500

    Update files2py.py

--- files2py.py
+++ files2py.py
@@ -20,25 +20,25 @@ if len(paths) != 0:
     fname = 'None.py' if fname == '' else fname
     fname += '.py'if fname[-3:] != '.py' else ''
     with open(fname, 'wb') as script_file:
-        script_file.write(bytes("file_names = [", encoding="utf-8"))
+        script_file.write("file_names = [".encode())
         for path in paths:
             while path.find('/') != -1:
                 path = path[path.find('/') + 1:]
-            script_file.write(bytes('"' + path + '", ', encoding="utf-8"))
-        script_file.write(bytes("]\n\n", encoding="utf-8"))
-        script_file.write(bytes("byte_files = [", encoding="utf-8"))
+            script_file.write(('"' + path + '", ').encode())
+        script_file.write("]\n\n".encode())
+        script_file.write("byte_files = [".encode())
         for file in paths:
             bytes_of_file = []
             with open(file, 'rb') as file_in_bytes:
                 for byte in file_in_bytes:
                     bytes_of_file.append(byte)
-            script_file.write(bytes(str(bytes_of_file) + ", ", encoding="utf-8"))
+            script_file.write((str(bytes_of_file) + ", ").encode())
 
-        script_file.write(bytes("]\n\n", encoding="utf-8"))
-        script_file.write(bytes("for i in range(len(file_names)):\n", encoding="utf-8"))
-        script_file.write(bytes("    with open(file_names[i], 'wb') as new_file:\n", encoding="utf-8"))
-        script_file.write(bytes("        for byte_file in byte_files[i]:\n", encoding="utf-8"))
-        script_file.write(bytes("            new_file.write(byte_file)\n", encoding="utf-8"))
+        script_file.write("]\n\n".encode("utf-8"))
+        script_file.write("for i in range(len(file_names)):\n".encode())
+        script_file.write("    with open(file_names[i], 'wb') as new_file:\n".encode())
+        script_file.write("        for byte_file in byte_files[i]:\n".encode())
+        script_file.write("            new_file.write(byte_file)\n".encode())
 else:
     print('No path was given!')
 input('Press Enter to finish script...')
